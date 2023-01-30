9f6aec0e424225146dbb64665c77e24ca66d8020
Oscar Li <lioscar58@gmail.com>
2023-01-30 12:41:45 -0500

    fixed visability

--- myFunction.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-  var x = document.getElementById("abMe");
-  var displaySettings = x.style.display;
-  if (displaySettings === "none") {
-    displaySettings = "inline-block";
-  } else {
-    displaySettings = "none";
-  }
-}
